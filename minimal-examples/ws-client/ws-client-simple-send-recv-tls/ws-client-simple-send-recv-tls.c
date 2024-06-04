2ebc085ec0017dc26b2dbcd383fe17c67706cd99
zzblydia <13098426+zzblydia@users.noreply.github.com>
2024-06-05 05:26:46 +0800

    update ws-client-simple-send-recv-tls.c && ws-server-simple-send-recv-tls.c

--- minimal-examples/ws-client/ws-client-simple-send-recv-tls/ws-client-simple-send-recv-tls.c
+++ minimal-examples/ws-client/ws-client-simple-send-recv-tls/ws-client-simple-send-recv-tls.c
@@ -9,19 +9,26 @@ void signal_handle(int sig) {
     exit_sig = 1;
 }
 
-typedef struct session_data {
+struct session_data {
     int msg_count;
     unsigned char buf[LWS_PRE + MAX_PAYLOAD_SIZE];
     int len;
-} SessionData;
+};
 
+/**
+ * 某个协议下的连接发生事件时，执行的回调函数
+ * wsi：指向webSocket实例的指针
+ * reason：导致回调的事件类型
+ * user 为每个WebSocket会话分配的内存空间
+ * in 某些事件使用此参数，作为传入数据的指针
+ * len 某些事件使用此参数，说明传入数据的长度
+ */
 int client_simple_callback(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len) {
     struct session_data *data = (struct session_data *) user;
     switch (reason) {
         case LWS_CALLBACK_CLIENT_ESTABLISHED:   // 连接到服务器后的回调
             lwsl_notice("Connected to server ok!\n");
             break;
-
         case LWS_CALLBACK_CLIENT_RECEIVE:       // 接收到服务器数据后的回调，数据为in，其长度为len
             lwsl_notice("receive: %s\n", (char *) in);
             break;
@@ -32,7 +39,6 @@ int client_simple_callback(struct lws *wsi, enum lws_callback_reasons reason, vo
                 char *msg = (char *) &data->buf[LWS_PRE];
                 data->len = sprintf(msg, "hello %d", data->msg_count);
                 lwsl_notice("send: %s\n", msg);
-                lwsl_wsi_notice(wsi, "send: %s\n", msg);
 
                 // 通过WebSocket发送文本消息
                 lws_write(wsi, &data->buf[LWS_PRE], (size_t) data->len, LWS_WRITE_TEXT);
@@ -46,6 +52,11 @@ int client_simple_callback(struct lws *wsi, enum lws_callback_reasons reason, vo
     return 0;
 }
 
+/**
+ * 支持的WebSocket子协议数组
+ * 子协议即JavaScript客户端WebSocket(url, protocols)第2参数数组的元素
+ * 你需要为每种协议提供回调函数
+ */
 struct lws_protocols protocols[] = {
         {
             //协议名称，协议回调，接收缓冲区大小
@@ -63,11 +74,9 @@ int main() {
     ctx_info.port = CONTEXT_PORT_NO_LISTEN;
     ctx_info.iface = NULL;
     ctx_info.protocols = protocols;
-    // ctx_info.gid = -1;
-    // ctx_info.uid = -1;
-    ctx_info.options |= LWS_SERVER_OPTION_DO_SSL_GLOBAL_INIT;
-
+    ctx_info.options |= LWS_SERVER_OPTION_DO_SSL_GLOBAL_INIT; // 如果使用SSL加密，需要设置此选项
     struct lws_context *context = lws_create_context(&ctx_info);
+
     char address[] = "127.0.0.1";
     int port = 8001;
     char addr_port[256] = {0};
@@ -86,12 +95,15 @@ int main() {
     conn_info.ssl_connection =
             LCCSCF_USE_SSL | LCCSCF_ALLOW_SELFSIGNED | LCCSCF_SKIP_SERVER_CERT_HOSTNAME_CHECK | LCCSCF_ALLOW_INSECURE;
 
-    // 下面的调用触发LWS_CALLBACK_PROTOCOL_INIT事件
     // 创建一个客户端连接
     struct lws *wsi = lws_client_connect_via_info(&conn_info);
 
     while (!exit_sig) {
         lws_service(context, 0);
+        /**
+         * 下面的调用的意义是：当连接可以接受新数据时，触发一次WRITEABLE事件回调
+         * 当连接正在后台发送数据时，它不能接受新的数据写入请求，所有WRITEABLE事件回调不会执行
+         */
         lws_callback_on_writable(wsi);
     }
     // 销毁上下文对象
---
+++ minimal-examples/ws-server/ws-server-simple-send-recv-tls/README.md
@@ -0,0 +1,6 @@
+# copyright   
+code sourced from the Internet    
+maybe https://www.goeasy.io/articles/803.html
+
+## 说明
+demo尽可能简单的演示基本的消息收发  
